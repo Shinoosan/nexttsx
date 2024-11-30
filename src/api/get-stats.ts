a5673163bd4c0a544e92f02878e304894b452b2f
Shinoosan <168342216+Shinoosan@users.noreply.github.com>
2024-11-30 13:47:30 +0530

    Update get-stats.ts

--- src/api/get-stats.ts
+++ src/api/get-stats.ts
@@ -1,8 +1,9 @@
 // /api/get-stats.ts
 import { prisma } from '@/lib/prisma';
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {  if (req.method === 'GET') {
     const { userId } = req.query;
 
     try {
