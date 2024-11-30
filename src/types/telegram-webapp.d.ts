ed7ba1327a9983566aeb83d4b261a665bd6df3c2
Shinoosan <168342216+Shinoosan@users.noreply.github.com>
2024-11-30 15:59:06 +0530

    Update telegram-webapp.d.ts

--- src/types/telegram-webapp.d.ts
+++ src/types/telegram-webapp.d.ts
@@ -1,23 +1,22 @@
-declare global {
-  interface TelegramWebApp {
-    ready(): void;
-    expand(): void;
-    initDataUnsafe: {
-      user?: {
-        id: number;
-        first_name: string;
-        last_name?: string;
-        username?: string;
-        language_code?: string;
-      };
+// types/telegram-web-app.d.ts
+interface TelegramWebApp {
+  ready: () => void;
+  expand: () => void;
+  initDataUnsafe: {
+    user?: {
+      id: number;
+      first_name: string;
+      last_name?: string;
+      username?: string;
     };
-  }
+    query_id?: string;
+  };
+}
 
+declare global {
   interface Window {
     Telegram: {
       WebApp: TelegramWebApp;
     };
   }
 }
-
-export {};
