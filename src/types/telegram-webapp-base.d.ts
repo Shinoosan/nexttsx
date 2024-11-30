191bbc8c89e6ff44ab86bd331a2c17a97afaedac
Shinoosan <168342216+Shinoosan@users.noreply.github.com>
2024-11-30 15:51:55 +0530

    Create telegram-webapp-base.d.ts

---
+++ src/types/telegram-webapp-base.d.ts
@@ -0,0 +1,50 @@
+interface WebAppUser {
+  id: number;
+  is_bot?: boolean;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code?: string;
+  is_premium?: boolean;
+  added_to_attachment_menu?: boolean;
+  allows_write_to_pm?: boolean;
+  photo_url?: string;
+}
+
+interface WebAppChat {
+  id: number;
+  type: "group" | "supergroup" | "channel";
+  title: string;
+  username?: string;
+  photo_url?: string;
+}
+
+interface WebAppInitData {
+  query_id?: string;
+  user?: WebAppUser;
+  receiver?: WebAppUser;
+  chat?: WebAppChat;
+  chat_type?: "sender" | "private" | "group" | "supergroup" | "channel";
+  chat_instance?: string;
+  start_param?: string;
+  can_send_after?: number;
+  auth_date: number;
+  hash: string;
+  signature?: string;
+}
+
+export interface WebApp {
+  ready(): void;
+  expand(): void;
+  initData: string;
+  initDataUnsafe: WebAppInitData;
+  MainButton: {
+    show(): void;
+    hide(): void;
+    setText(text: string): void;
+    onClick(fn: () => void): void;
+  };
+  platform: string;
+  version: string;
+  colorScheme: string;
+} 
