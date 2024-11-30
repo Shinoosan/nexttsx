93929d84090e62c94696018b72a55ebdcd7d13ad
Shinoosan <168342216+Shinoosan@users.noreply.github.com>
2024-11-30 13:16:42 +0530

    Update telegram.ts

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,6 +1,6 @@
 // src/lib/telegram.ts
 import TelegramBot from 'node-telegram-bot-api';
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN!, { polling: false });
+const bot = new TelegramBot(process.env.NEXT_PUBLIC_BOT_TOKEN!, { polling: false });
 
 export { bot };
