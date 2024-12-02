8a708c1511f87db5c1b9a7bf6d43fbcb2015ff58
Shinoosan <168342216+Shinoosan@users.noreply.github.com>
2024-12-02 22:15:17 +0530

    Update telegram-webapp.d.ts

--- src/types/telegram-webapp.d.ts
+++ src/types/telegram-webapp.d.ts
@@ -1,22 +1,41 @@
-// types/telegram-web-app.d.ts
-interface TelegramWebApp {
-  ready: () => void;
-  expand: () => void;
-  initDataUnsafe: {
-    user?: {
-      id: number;
-      first_name: string;
-      last_name?: string;
-      username?: string;
-    };
-    query_id?: string;
-  };
-}
-
 declare global {
   interface Window {
     Telegram: {
-      WebApp: TelegramWebApp;
+      WebApp: {
+        initDataUnsafe: {
+          query_id?: string;
+          user?: {
+            id: number;
+            is_bot?: boolean;
+            first_name: string;
+            last_name?: string;
+            username?: string;
+            language_code?: string;
+            is_premium?: boolean;
+            added_to_attachment_menu?: boolean;
+            allows_write_to_pm?: boolean;
+            photo_url?: string;
+          };
+          receiver?: {
+            id: number;
+            is_bot?: boolean;
+            first_name: string;
+            last_name?: string;
+            username?: string;
+          };
+          chat?: {
+            id: number;
+            type: string;
+            title: string;
+          };
+          chat_type?: 'sender' | 'private' | 'group' | 'supergroup' | 'channel';
+          chat_instance?: string;
+          start_param?: string;
+        };
+        ready: () => void;
+      };
     };
   }
 }
+
+export {};
