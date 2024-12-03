48dd75140ac1fd9c44c6284fbdf818afe8c3d924
Shinoosan <168342216+Shinoosan@users.noreply.github.com>
2024-12-03 22:33:19 +0530

    qs

--- src/components/views/profile-view.tsx
+++ src/components/views/profile-view.tsx
@@ -16,6 +16,17 @@ interface Stats {
   }
 }
 
+interface TelegramUser {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code?: string;
+  is_premium?: boolean;
+  photo_url?: string;
+}
+
+
 interface ProfileViewProps {
   telegramUserId: string;
   processedCount: number;
