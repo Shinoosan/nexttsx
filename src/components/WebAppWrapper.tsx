2b49e0ebc2df6eae29a0fcb9af6d496fec938a17
kxSharma <kunalksharma086@gmail.com>
2024-12-04 01:30:48 +0530

    hbkh

--- src/components/WebAppWrapper.tsx
+++ src/components/WebAppWrapper.tsx
@@ -16,9 +16,25 @@ export function WebAppWrapper({ children }: WebAppWrapperProps) {
       
       // Emit user data to parent component if available
       if (WebApp.initDataUnsafe?.user) {
+        const user = WebApp.initDataUnsafe.user;
+        const transformedUser: WebAppUser = {
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          username: user.username,
+          language_code: user.language_code,
+          is_premium: user.is_premium,
+          // Add the transformed fields
+          firstName: user.first_name,
+          lastName: user.last_name,
+          languageCode: user.language_code,
+          isPremium: user.is_premium,
+          photoUrl: user.photo_url
+        };
+
         window.dispatchEvent(
           new CustomEvent<WebAppUser>('webappUserData', {
-            detail: WebApp.initDataUnsafe.user
+            detail: transformedUser
           })
         );
       }
